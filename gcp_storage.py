fb56d5a68d97948f116d29be86b27ec6f72ae894
shjoin <shjoin@gmail.com>
2022-05-04 00:21:37 -0500

    added

--- gcp_storage.py
+++ gcp_storage.py
@@ -5,17 +5,27 @@ from utils import *
 
 
 if __name__ == "__main__":   
-    confobj =get_spark_app_config()
+    '''confobj =get_spark_app_config()
     spark = SparkSession.builder \
            .config(conf=confobj) \
            .getOrCreate()
+'''
+    spark = SparkSession \
+        .builder \
+        .appName("gcp_storage") \
+        .master("local[1]") \
+        .getOrCreate()
 
-    conf_out = spark.sparkContext.getConf()        
-    file_name='C:/github/mypyspark-shjo/data'
-    file_df= read_csv_df(spark,file_name)
+    print('filename shjo2')
+
+    #conf_out = spark.sparkContext.getConf()        
+    file_name='C:/github/mypyspark-shjo/data/sample.csv'
+    print('filename shjo')
+    file_df = read_csv_df(spark, file_name)
     #file_df.show(10)
       
     file_df.createOrReplaceTempView("survey_tbl")
     countDF = spark.sql("select Country, count(1) as count from survey_tbl where Age<40 group by Country")
 
     countDF.show()
+  
