612b3ffa3ffb19b73d5331e994ab42855598b9cb
shjoin <shjoin@gmail.com>
2022-05-04 13:40:13 -0500

    added2

--- gcp_storage.py
+++ gcp_storage.py
@@ -5,22 +5,15 @@ from utils import *
 
 
 if __name__ == "__main__":   
-    '''confobj =get_spark_app_config()
+    confobj =get_spark_app_config()
     spark = SparkSession.builder \
            .config(conf=confobj) \
-           .getOrCreate()
-'''
-    spark = SparkSession \
-        .builder \
-        .appName("gcp_storage") \
-        .master("local[1]") \
-        .getOrCreate()
-
-    print('filename shjo2')
+           .getOrCreate()    
 
     #conf_out = spark.sparkContext.getConf()        
     file_name='C:/github/mypyspark-shjo/data/sample.csv'
-    print('filename shjo')
+    #file_name='gs://gcp-storage-bucket-shjo/sample.csv'
+    print('Reading Starts......')
     file_df = read_csv_df(spark, file_name)
     #file_df.show(10)
       
@@ -28,4 +21,4 @@ if __name__ == "__main__":
     countDF = spark.sql("select Country, count(1) as count from survey_tbl where Age<40 group by Country")
 
     countDF.show()
-  
+    print('Reading complete ......')
---
+++ gcp_storage_pandas.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import datetime
+
+file_path ='C:/github/mypyspark-shjo/data'
+file_name ='sample.csv'
+file_name_out='sample_out'+ '_' + datetime.datetime.now().strftime("%Y%m%d") + '_' + datetime.datetime.now().strftime("%H%M%S") + '.csv'
+#file_name='gs://gcp-storage-bucket-shjo/sample.csv'
+
+pd_df=pd.read_csv(file_path + '/' + file_name)
+print("FIle read")
+#pd_df.head()
+
+#new_pd_df=pd_df.where(pd_df["Age"] < 40).groupby (['Country'])['Country'].count()
+new_pd_df=pd_df.where(pd_df["Age"] < 40).groupby (['Country'])['Age'].aggregate(['min','max'])
+new_pd_df=pd_df.where(pd_df["Age"] < 40).groupby (['Country']).aggregate({'name':'count','Age':['min','max']})
+
+print("Aggregated")
+new_pd_df.to_csv(file_path + '/' + file_name_out)
+print("FIle Saved")
+pd_df_read=pd.read_csv(file_path + '/' + file_name_out)
+pd_df_read.head()
